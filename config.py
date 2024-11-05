f8ff26120860e3a1b98472d929d5b223649f9add
conradlempert <conradlempert@gmail.com>
2024-11-05 15:20:11 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 COM_PORT = "" # Enter your COM_PORT here
 
-# Typical Mac COM PORT: "/dev/tty.SLAB_USBtoUART"
+# Typical Mac COM PORT: "/dev/cu.SLAB_USBtoUART"
 # Typical Windows COM PORT: "//.//COM3"
 # Typical Linux COM PORT: "/dev/ttyUSB0"
 
